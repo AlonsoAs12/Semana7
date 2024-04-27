7b1028ed4b5ba3032ec323c5015c3700dc38cd38
Alumno <Alumno@LAB1504-24>
2024-04-27 12:55:58 -0500

    Agregar archivos de proyecto.

---
+++ Business/CustomerBusiness.cs
@@ -0,0 +1,28 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+
+namespace Business
+{
+    public class CustomerBusiness
+    {
+        private readonly Data.CustomerData customerData;
+
+        public CustomerBusiness(string connectionString)
+        {
+            customerData = new Data.CustomerData(connectionString);
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return customerData.GetCustomers();
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            return customerData.GetCustomerById(customerId);
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            customerData.AddCustomer(customer
---
+++ Data/CustomerData.cs
@@ -0,0 +1,36 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public List<Customer> GetCustomers()
+    {
+        List<Customer> customers = new List<Customer>();
+
+        using (SqlConnection connection = new SqlConnection("LAB1504-31"))
+        {
+            SqlCommand command = new SqlCommand("GetCustomersProc", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Customer customer = new Customer
+                {
+                    CustomerId = (int)reader["customer_id"],
+                    Name = reader["name"].ToString(),
+                    Address = reader["address"].ToString(),
+                    Phone = reader["phone"].ToString(),
+                    Active = (bool)reader["active"]
+                };
+                customers.Add(customer);
+            }
+        }
+
+        return customers;
+    }
+}
